68172867fbd81f9ab25ab72adb255e59c17ebe48
dushyant22 <dushy_ant@hotmail.com>
2018-07-25 14:51:41 +0530

    First Commit

---
+++ app/src/main/java/update/bebitz/andapp3/connectionClass.java
@@ -0,0 +1,13 @@
+package update.bebitz.andapp3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connectionClass {
+    String strIP = "192.168.111.2\\SQLExpress";
+    String strDB = "ABASReport";
+    String strUser = "dushy";
+    String strPass = "sardar";
+
+}
